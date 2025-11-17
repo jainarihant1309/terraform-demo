001ad75a1bbdd5196bdbe045fbe0281a90113d3a
jainarihant1309 <jainarihant1309@gmail.com>
2025-11-17 16:41:59 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,23 @@
 
+terraform {
+  required_version = ">= 1.0"
+
+  backend "s3" {
+    bucket = "your-tf-state-bucket"
+    key    = "dev/terraform.tfstate"
+    region = "ap-south-1"
+  }
+}
+
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_instance" "demo" {
+  ami           = "ami-0f8ca728008ff5af4"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "github-actions-demo"
+  }
+}
